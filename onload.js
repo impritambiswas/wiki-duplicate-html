73dbfc6fa2399c46db24504355574c0ffba8a7c3
Pritam Biswas <121305448+impritambiswas@users.noreply.github.com>
2024-05-01 11:45:49 +0000

    deleted onload.js, related files and snippets

--- onload.js
+++
@@ -1,6 +0,0 @@
-function linkNewAccount() {
-    let newAcc = document.getElementsByClassName('create-account');
-    if(newAcc.href = '#') {
-        newAcc.setAttribute('href', 'https://en.wikipedia.org/w/index.php?title=Special:CreateAccount&returnto=HTML');
-    }
-}
